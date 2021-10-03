b0b751ff2de8141787615a8985cadb8659358647
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2021-10-02 19:25:55 -0500

    Tweed card completed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 const $likeBtn = document.querySelector('.icon-heart');
+const $retweetBtn = document.querySelector('.icon-retweet');
 
 $likeBtn.addEventListener('click', (e) => {
+	tweetEvent(e);
+});
+
+$retweetBtn.addEventListener('click', (e) => {
+	tweetEvent(e, false);
+});
+
+function tweetEvent(e, isChild = true) {
 	const isLiked = e.target.parentElement.classList.toggle('is-liked');
 
-	const counter = e.target.nextElementSibling;
+	const counter = isChild
+		? e.target.nextElementSibling
+		: e.target.parentElement.nextElementSibling;
 
 	counter.textContent = isLiked
 		? parseInt(counter.textContent) + 1
 		: parseInt(counter.textContent) - 1;
-});
+}
